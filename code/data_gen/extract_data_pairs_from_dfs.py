7b7ecb97e9b755a061ca6e80ca28faa4703d46d6
Ahmad Rezaie <ahmadreza.rezaie75@gmail.com>
2024-08-01 14:22:39 -0300

    phase 8 1.5k added

--- code/data_gen/extract_data_pairs_from_dfs.py
+++ code/data_gen/extract_data_pairs_from_dfs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-directory = "/Users/ahmadrezaie/papers/Synthetic_Data_Gen/data/output/notes_onVector/phase_7_1.5k/"
+directory = "/Users/ahmadrezaie/papers/Synthetic_Data_Gen/data/output/notes_onVector/phase_8_1.5k/"
 
 all_files = os.listdir(directory)
 
