49f54a0d81fd7ededa0052d2266fe2b0eab87e7d
Ahmad Rezaie <ahmadreza.rezaie75@gmail.com>
2024-08-05 09:10:53 -0300

    phase 10 notes added

--- code/data_gen/generate_notes.py
+++ code/data_gen/generate_notes.py
@@ -29,7 +29,7 @@ def file_exists(disease_description, path_to_save_notes):
 
 def main():
     ###### Edit this for each phase #############
-    path_to_save_notes= "/Users/ahmadrezaie/papers/Synthetic_Data_Gen/data/output/notes_onVector/phase_9_1.5k"
+    path_to_save_notes= "/Users/ahmadrezaie/papers/Synthetic_Data_Gen/data/output/notes_onVector/phase_10_1.5k"
     ##############################################
     counter= 1
     openai_client= note_generation_functions.initialize_openai_client()
@@ -41,7 +41,8 @@ def main():
     #next_100_icd10_desc = df['ICD10_desc'].iloc[400:500].tolist()
     #next_300_icd10_desc = df['ICD10_desc'].iloc[500:800].tolist()
     #next_300_icd10_desc = df['ICD10_desc'].iloc[800:1100].tolist()  
-    next_300_icd10_desc = df['ICD10_desc'].iloc[1100:1400].tolist()  
+    #next_300_icd10_desc = df['ICD10_desc'].iloc[1100:1400].tolist()  
+    next_300_icd10_desc = df['ICD10_desc'].iloc[1400:1700].tolist()  
 
 
     for disease in next_300_icd10_desc:
